9d30317a5ba20b1360b5c30e83d754c747b77f85
tatoshvili <giogiololpro@gmail.com>
2025-02-24 12:06:52 +0400

    Add files via upload

---
+++ day2/day2.py
@@ -0,0 +1,14 @@
+name = "giorgi"
+surname = "jokiladze"
+
+print(name)
+
+name = "giorgi"
+age = 15
+height = 180
+
+knows_programing = False
+knows_how_to_cooking = True
+
+print(name + " " + surname)
+print(name + " " + str(age))
